b69f20eb9b458924afeaac40a2127fadd39bd3ea
Mikhail Lapin <mlapin@rambler.ru>
2022-02-09 15:32:33 +0300

    mod3_solution

--- sites/l40/js/script.js
+++ sites/l40/js/script.js
@@ -1,14 +1,27 @@
-var message = "in global";
-console.log('global: message = ' + message);
-var a = function () { 
-	var message = "inside a"; 
-    console.log("a: message = " + message); 
-	b();
-}
+// Event handling
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    
+    // Unobtrusive event binding
+    document.querySelector("button")
+      .addEventListener("click", function () {
+        
+        // Call server to get the name
+        $ajaxUtils
+          .sendGetRequest("data/name.txt", 
+            function (request) {
+              var name = request.responseText;
 
-function b () { 
-	console.log('b: message = ' + message)
-}
+              document.querySelector("#content")
+                .innerHTML = "<h2>Hello " + name + "!</h2>";
+            });
 
-a()
+        
+      });
+  }
+);
+
+
+
+
 
